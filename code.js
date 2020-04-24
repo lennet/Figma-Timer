7765acadd476782bc10c1e55bd53fbb36cf7e4d0
Jannes Peters <jannesp@gmail.com>
2020-04-24 15:14:39 +0200

    added a pause button
    also added code for a progress bar, but not finished yet, so commented that out.

--- code.js
+++ code.js
@@ -11,23 +11,51 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 var activeTimer = 0;
 const secondsSet = [86400, 3600, 60, 1];
 const progressBar = figma.createRectangle();
-if (checkForSelectedNodes() == false) {
-    if (checkForNodesThatBeginWithTimer() == false) {
-        throw new Error("Type the time to start Timer");
+var pause = 0;
+var userSetSeconds = 0;
+figma.showUI(__html__, { width: 100, height: 50 });
+figma.ui.onmessage = msg => {
+    if (msg.type === 'start') {
+        pause = 0;
+        console.log("start message from UI");
+        checkAndStart();
+    }
+    if (msg.type === 'pause') {
+        console.log("pause message from UI");
+        if (pause > 0) {
+            pause = 0;
+            console.log("unpause");
+            checkAndStart();
+        }
+        else {
+            pause = 1;
+        }
+    }
+    // figma.closePlugin();
+};
+function checkAndStart() {
+    if (checkForSelectedNodes() == false) {
+        console.log("This is the first if-thing in Leos code");
+        if (checkForNodesThatBeginWithTimer() == false) {
+            throw new Error("Type the time to start Timer");
+        }
     }
 }
 function checkForSelectedNodes() {
+    console.log("checkforSelectedNodes");
     const regex = new RegExp("[0-9]{1,2}(:[0-9]{1,2})*");
     const selectedNodes = figma.currentPage.selection.filter(node => node.type == "TEXT" && regex.test(node.characters));
     selectedNodes.forEach(start);
     return selectedNodes.length > 0;
 }
 function checkForNodesThatBeginWithTimer() {
+    console.log("checkforNodesThatBeginWithTimer");
     const nodes = figma.currentPage.findAll(node => node.type === "TEXT" && node.characters.startsWith("Timer:"));
     nodes.forEach(start);
     return nodes.length > 0;
 }
 function start(node) {
+    console.log("start function");
     var timeString = node.characters;
     var startsWithTimer = false;
     if (timeString.startsWith("Timer:")) {
@@ -39,6 +67,7 @@ function start(node) {
     startTimer(node, seconds, template, startsWithTimer);
 }
 function getRemainingSeconds(timeString) {
+    console.log("getRemainingSeconds");
     var seconds = 0;
     var components = timeString.split(":");
     secondsSet.reverse();
@@ -54,6 +83,7 @@ function getRemainingSeconds(timeString) {
  * e.g. converts 5:00 into 0:00
  */
 function getTeamplateFromString(timeString) {
+    console.log("getTemplateFromString");
     var result = "";
     for (const c of timeString) {
         if (c == ":") {
@@ -70,6 +100,7 @@ function getTeamplateFromString(timeString) {
 * e.g. 5:00 (timeString) and 00:00:00 (template) will return 00:05:00
 */
 function fillUpTimeStringWithTempalte(timeString, template) {
+    console.log("fillUpTimeStringWithTemplate");
     const trimmedTemplate = template.substring(0, template.length - timeString.length);
     return trimmedTemplate + timeString;
 }
@@ -99,7 +130,7 @@ function createProgressBar() {
     nodes.push(progressBar);
 }
 function runProgressBar(secondsToGo) {
-    console.log(secondsToGo + " this is secondsToGo");
+    //console.log(secondsToGo + " this is secondsToGo");
     if (secondsToGo > 0) {
         progressBar.resizeWithoutConstraints(secondsToGo, 50);
     }
@@ -112,20 +143,28 @@ function startTimer(node, seconds, template, startsWithTimer) {
         yield figma.loadFontAsync(node.fontName);
         activeTimer += 1;
         console.log("Timer started / became active");
-        createProgressBar();
+        //Jannes is also working in a progress bar feature. To be finished later.
+        //createProgressBar();
         var secondsToGo = seconds;
         while (secondsToGo > 0) {
-            var newText = fillUpTimeStringWithTempalte(secondsToInterval(secondsToGo), template);
-            if (startsWithTimer) {
-                newText = "Timer: " + newText;
+            if (pause == 0) {
+                var newText = fillUpTimeStringWithTempalte(secondsToInterval(secondsToGo), template);
+                if (startsWithTimer) {
+                    newText = "Timer: " + newText;
+                }
+                node.characters = newText;
+                yield delay(1000);
+                secondsToGo -= 1;
+                runProgressBar(secondsToGo);
             }
-            node.characters = newText;
-            yield delay(1000);
-            secondsToGo -= 1;
-            runProgressBar(secondsToGo);
+            else {
+                secondsToGo = 0;
+            }
         }
-        node.characters = "Done";
-        activeTimer -= 1;
+        if (pause == 0) {
+            node.characters = "Done";
+            activeTimer -= 1;
+        }
         console.log("Timer finsihed / became in-active");
         if (activeTimer <= 0) {
             figma.closePlugin();
--- code.ts
+++ code.ts
@@ -2,14 +2,50 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 var activeTimer = 0;
 const secondsSet = [86400, 3600, 60, 1];
 const progressBar = figma.createRectangle();
+var pause = 0;
+var userSetSeconds = 0;
 
-if (checkForSelectedNodes() == false) {
-  if (checkForNodesThatBeginWithTimer() == false) {
-    throw new Error("Type the time to start Timer");
+figma.showUI(__html__, { width: 100, height: 50 })
+
+figma.ui.onmessage = msg => {
+ 
+  if (msg.type === 'start') {
+    pause = 0;
+    console.log ("start message from UI");
+    checkAndStart();
+  }
+
+  if (msg.type === 'pause') {
+    console.log ("pause message from UI");
+
+    if (pause > 0) {
+      pause = 0;
+      console.log ("unpause");
+      checkAndStart();
+    } else {
+      pause = 1;
+    }
+  }
+
+  // figma.closePlugin();
+};
+
+function checkAndStart()
+{
+  if (checkForSelectedNodes() == false) {
+
+    console.log ("This is the first if-thing in Leos code");
+  
+    if (checkForNodesThatBeginWithTimer() == false) {
+      throw new Error("Type the time to start Timer");
+    }
   }
 }
 
 function checkForSelectedNodes() : boolean {
+
+  console.log ("checkforSelectedNodes");
+
   const regex = new RegExp("[0-9]{1,2}(:[0-9]{1,2})*");
   const selectedNodes =  figma.currentPage.selection.filter(node => node.type == "TEXT" && regex.test(node.characters));
   selectedNodes.forEach(start);
@@ -17,12 +53,18 @@ function checkForSelectedNodes() : boolean {
 }
 
 function checkForNodesThatBeginWithTimer() : boolean {
+
+  console.log ("checkforNodesThatBeginWithTimer");
+
   const nodes = figma.currentPage.findAll(node => node.type === "TEXT" && node.characters.startsWith("Timer:"));
   nodes.forEach(start);
   return nodes.length > 0;
 }
 
 function start(node: TextNode) {
+
+  console.log ("start function");
+
   var timeString = node.characters;
   var startsWithTimer = false;
   if (timeString.startsWith("Timer:")) {
@@ -37,6 +79,9 @@ function start(node: TextNode) {
 }
 
 function getRemainingSeconds(timeString: string) : number {
+
+  console.log ("getRemainingSeconds");
+
   var seconds = 0;
   var components = timeString.split(":");
   secondsSet.reverse();
@@ -56,6 +101,9 @@ function getRemainingSeconds(timeString: string) : number {
  * e.g. converts 5:00 into 0:00
  */
 function getTeamplateFromString(timeString: string) : string {
+
+  console.log ("getTemplateFromString");
+
   var result = "";
   for (const c of timeString) {
     if (c == ":") {
@@ -72,6 +120,9 @@ function getTeamplateFromString(timeString: string) : string {
 * e.g. 5:00 (timeString) and 00:00:00 (template) will return 00:05:00
 */
 function fillUpTimeStringWithTempalte(timeString: string, template: string) : string {
+
+  console.log ("fillUpTimeStringWithTemplate");
+
   const trimmedTemplate = template.substring(0, template.length - timeString.length) 
   return trimmedTemplate + timeString;
 }
@@ -107,7 +158,7 @@ function createProgressBar(){
 }
 
 function runProgressBar(secondsToGo: number){
-  console.log(secondsToGo + " this is secondsToGo");
+  //console.log(secondsToGo + " this is secondsToGo");
 
   if (secondsToGo > 0) {
     progressBar.resizeWithoutConstraints(secondsToGo, 50);
@@ -123,24 +174,36 @@ async function startTimer(node: TextNode, seconds: number, template: string, sta
   activeTimer += 1;
 
   console.log("Timer started / became active");
-
-  createProgressBar();
+  
+  //Jannes is also working in a progress bar feature. To be finished later.
+  //createProgressBar();
 
   var secondsToGo = seconds;
-  while(secondsToGo > 0) {
-    var newText = fillUpTimeStringWithTempalte(secondsToInterval(secondsToGo), template);
-    if (startsWithTimer) {
-        newText = "Timer: " + newText;
-    }
-    node.characters = newText;
-    await delay(1000);
-    secondsToGo -= 1;
+  
+  
 
-    runProgressBar(secondsToGo);
+
+  while(secondsToGo > 0) {
+
+    if (pause == 0){
+      var newText = fillUpTimeStringWithTempalte(secondsToInterval(secondsToGo), template);
+      if (startsWithTimer) {
+          newText = "Timer: " + newText;
+      }
+      node.characters = newText;
+      await delay(1000);
+      secondsToGo -= 1;
+
+      runProgressBar(secondsToGo);
+    } else {
+      secondsToGo = 0;
+    }
   }
 
-  node.characters = "Done";
-  activeTimer -= 1;
+  if (pause == 0){
+    node.characters = "Done";
+    activeTimer -= 1;
+  }
 
   console.log("Timer finsihed / became in-active");
 
