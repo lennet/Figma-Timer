41970dcb0e12aa4e084d5fdf998edd72659c24d0
Jannes Peters <jannesp@gmail.com>
2020-05-07 09:48:37 +0200

    Merged from master and updated UI window size

--- code.js
+++ code.js
@@ -32,7 +32,7 @@ figma.ui.onmessage = msg => {
             pause = true;
             break;
         case 'helpon':
-            figma.ui.resize(220, 200);
+            figma.ui.resize(220, 220);
             break;
         case 'helpoff':
             figma.ui.resize(220, 50);
